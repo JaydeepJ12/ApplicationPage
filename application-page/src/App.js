baccfe3f7f179c5270ad7fd2a6fee4639e1ea614
Michael Fore <MichaelAF@BoxerProperty.com>
2020-12-11 14:07:32 -0600

    glassware compoents

--- application-page/src/App.js
+++ application-page/src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import "./App.css";
 import CaseCreator from "./components/case_creator.js";
 import Dashboard from "./components/dashboard.js";
+import GlassBox from "./components/glassware";
 import OverView from "./components/overview";
 import theme from "./components/theme.js";
 const useStyles = makeStyles((theme) => ({
@@ -12,17 +13,18 @@ const useStyles = makeStyles((theme) => ({
     backgroundImage: `url(https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png)`,
   },
 }));
-
+//<Dashboard></Dashboard>
 function App() {
   const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
       <div classes={classes.root}></div>
-      <Dashboard></Dashboard>
+      
       <div>
         <Router>
           <CaseCreator path="case-create" />
           <OverView path="overview" />
+          <GlassBox path="glass"></GlassBox>
         </Router>
       </div>
     </ThemeProvider>
