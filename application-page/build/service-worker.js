af4703b697a23cfa5d09cb38bbbc00cf234995d1
Michael Fore <MichaelAF@BoxerProperty.com>
2020-12-15 14:39:24 -0600

    dev server deployment

--- application-page/build/service-worker.js
+++ application-page/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d12e52ff8d3c994e678d89dde01596b2.js"
+  "/react/precache-manifest.2c35cbfa846d0f778156efde7632d202.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
