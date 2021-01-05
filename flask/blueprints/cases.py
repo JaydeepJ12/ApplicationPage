fb260a15b1baeb25dc6f3ab744c0aee186c4f14e
BhavikS12 <Bhavik.Savaliya@Stemmons.com>
2021-01-05 20:02:42 +0530

    Changes related to Add Filters in View Case Page.

--- flask/blueprints/cases.py
+++ flask/blueprints/cases.py
@@ -88,17 +88,18 @@ def get_employees_by_search():
     data = mobile.get_employees_by_search(request.json).json()
     return data
 
-def getUserFullName(name_list):
-    df = db.get_user_fullname(name_list)#always returns dataframe
+def getUserFullName(userShortName):
+    df = db.get_user_fullname(userShortName) #always returns dataframe
     return df.to_json(orient='records')
-
+   
 @bp.route('/GetCaseNotes', methods=['POST'])
 def get_case_notes():
     data = mobile.get_case_notes(request.json).json()
     for x in data['responseContent']: #can throw error with resp is empty
         userShortName = x.get('createdBy')
         userFullName = json.loads(getUserFullName(str(userShortName)))
-        x['fullName'] = userFullName[0]['FULL_NAME']
+        if userFullName:
+            x['fullName'] = userFullName[0]['FULL_NAME']
         # x.update({'createdBy':userFullName[0]['FULL_NAME']}) 
     return data
 
@@ -108,23 +109,24 @@ def get_case_headers():
     return data
 
 def getSystemPriority(assocTypeId):
-    df = db.get_system_priority(assocTypeId)#always returns dataframe
+    df = db.get_system_priority(assocTypeId) #always returns dataframe
     return df.to_json(orient='records')
 
 @bp.route('/GetFullCaseByCaseId', methods=['POST'])
 def get_full_case_by_caseId():
     data = mobile.get_full_case_by_caseId(request.json).json()
-    for x in data['responseContent']['notes']: #can throw error with resp is empty
-        userShortName = x['createdBy']
-        userFullName = json.loads(getUserFullName(str(userShortName)))
-        x['fullName'] = userFullName[0]['FULL_NAME']
-    for y in data['responseContent']['details']: #can throw error with resp is empty
-        # assocDecodeData = assocDecode(y['controlId'])
-        # y['assoc_decode'] = json.loads(assocDecodeData)
-        systemPriority = json.loads(getSystemPriority(y['controlId']))
-        y['systemPriority'] = systemPriority[0]['SYSTEM_PRIORITY']
-        # y['assoc_decode'] = []
-        # y['systemPriority'] = 1
+    # for x in data['responseContent']['notes']: #can throw error with resp is empty
+    #     userShortName = x['createdBy']
+    #     print('userShortName --- ' , userShortName)
+    #     userFullName = json.loads(getUserFullName(str(userShortName)))
+    #     print('userFullName-----', userFullName)
+    #     if userFullName:
+    #         x['fullName'] = userFullName[0]['FULL_NAME']
+    # for y in data['responseContent']['details']: #can throw error with resp is empty
+    #     systemPriority = json.loads(getSystemPriority(y['controlId']))
+    #     y['systemPriority'] = systemPriority[0]['SYSTEM_PRIORITY']
+    #     # y['assoc_decode'] = []
+    #     # y['systemPriority'] = 1
     return data
 
 @bp.route('/assoc_type', methods=['GET'])
