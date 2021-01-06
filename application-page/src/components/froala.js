3e5898c6f7951cc52b231be137d1ad867e473496
BOXERPROPERTY\DixitMS <Dixit.Solanki@Stemmons.com>
2021-01-06 17:59:35 +0530

    Dixit  changes regard code clean on 6-1-2021

--- application-page/src/components/froala.js
+++ application-page/src/components/froala.js
@@ -1,19 +1,13 @@
 import "froala-editor/css/froala_editor.pkgd.min.css";
-// Require Editor CSS files.
 import "froala-editor/css/froala_style.min.css";
-// Require Editor plugin files.
 import "froala-editor/js/plugins.pkgd.min.js";
 import React from "react";
 import FroalaEditor from "react-froala-wysiwyg";
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
 
-
-// Require Font Awesome.
-// import 'font-awesome/css/font-awesome.css';
-
 export default function Froala(props) {
   return (
-    <div className="">
+    <div className="input-editor">
       <FroalaEditor
         {...props}
         tag="textarea"
@@ -24,10 +18,6 @@ export default function Froala(props) {
       />
     </div>
   );
-
-  // return ( <FroalaEditor
-  //     {...props}
-  //     config={{apiKey:'AVB8B-21A3B3E3D2F2D1ua2BD1IMNBUMRWAd1AYMSTRBUZYA-9H3E2J2C5C6B3B2B5B1D1=='}}/> )
 }
 
 function ViewFroala() {
