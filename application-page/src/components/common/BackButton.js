d9f4bf4fd546371df591d6141678d70ebc0b26b8
BOXERPROPERTY\DixitMS <Dixit.Solanki@Stemmons.com>
2021-01-19 18:46:26 +0530

    UI Formating Changes 19-1-2021

--- application-page/src/components/common/BackButton.js
+++ application-page/src/components/common/BackButton.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { navigate } from "@reach/router";
 import { Button } from "@material-ui/core";
 import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
+import { navigate } from "@reach/router";
+import React from "react";
 export default function GotoBackButton(props) {
   const handleRedirect = () => {
-    navigate(-1)
+    navigate(-1);
   };
   return (
-    <div className="btn-back">
+    <div className="btn-back st-p-2">
       <Button
         onClick={() => {
           handleRedirect();
