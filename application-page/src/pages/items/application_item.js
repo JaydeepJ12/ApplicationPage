6a9017475408ce4cd39c32898193736b0a464608
BOXERPROPERTY\DixitMS <Dixit.Solanki@Stemmons.com>
2021-03-10 17:52:48 +0530

    Item page releted work and people department releted fixes.....

--- application-page/src/pages/items/application_item.js
+++ application-page/src/pages/items/application_item.js
@@ -1,58 +1,46 @@
-import {
-  Box,
-  Grid,
-} from "@material-ui/core";
+import { Box, Grid, Card, CardHeader, CardContent } from "@material-ui/core";
 import axios from "axios";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
-import {
-  default as useStyles,
-} from "../../assets/css/common_styles";
+import { default as useStyles } from "../../assets/css/common_styles";
+import { useTheme } from "@material-ui/core/styles";
 import * as notification from "../../components/common/toast";
 
-import * as API from '../../components/api_base/path-config';
-
-import EntityMainTab from "./entity_main_tab";
-import BasicEntityInfo from "./entity_basic_information";
+import * as API from "../../components/api_base/path-config";
+import { useSelector } from "react-redux";
+import EntityInfoSection from "./entity_info_section";
 import ApplicationItemFilter from "./application_item_filter";
 import EntityCard from "./entity_card";
-
+import ComponentLoader from "../../components/common/component-loader";
 var dateFormat = require("dateformat");
 
 export default function PeopleDepartment() {
   var classes = useStyles();
-  var [value, setValue] = React.useState(0);
-  const [page, setPage] = React.useState(0);
-  const [rows, setRows] = React.useState([]);
+  const theme = useTheme();
   // all loading
   const [componentLoader, setComponentLoader] = useState(false);
-  const [taskLoader, setTaskLoader] = useState(false);
   const [dataLoaded, setDataLoaded] = useState(false);
   const [dataInfoLoaded, setInfoDataLoaded] = useState(false);
+  const [InfoCardHeaderText, setInfoCardHeaderText] = useState("");
   const [noDataFound, setNoDataFound] = useState(false);
-  const [loading, setLoading] = React.useState(false);
-  var [navTab, setNavTab] = useState(1);
-  // end  all loading
 
   // all data set
   const [entityList, setEntityList] = useState([]);
-  const [entityInfo, setEntityInfo] = useState("");
+  const [entityInfo, setEntityInfo] = useState([]);
   // end all data set
 
   // for Filters
   const [maxCount, setMaxCount] = useState(10);
-  const [activityLogMaxCount, setActivityLogMaxCount] = useState(100);
-  const [pageSize, setPageSize] = useState(10);
-  const [gridSkipCount, setGridSkipCount] = useState(0);
+  const [entitiesValue, setEntityIds] = useState(0);
 
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeightCard);
 
-  // Start  all API call
-  const getEntityList = async (
-    searchText = "",
-    skipCount = 0,
-    isScroll
-  ) => {
+  const reducerState = useSelector((state) => state);
+  const entityData = reducerState.applicationData.applicationElements.filter(
+    (x) => x.SYSTEM_CODE === "ASSET"
+  );
+
+  const getEntityList = async (skipCount = 0, EntityIds, isScroll) => {
     if (!isScroll) {
       setInfoDataLoaded(false);
     }
@@ -60,14 +48,10 @@ export default function PeopleDepartment() {
     setComponentLoader(true);
     setEntityList([]);
     setNoDataFound(false);
-    var jsonData = {
-      maxCount: maxCount + skipCount,
-      searchText: searchText,
-    };
+    var maxRecord = maxCount + skipCount;
     var config = {
-      method: "post",
-      url: '/cases/getDepartmentPeoples',
-      data: jsonData,
+      method: "get",
+      url: "/entity/list_by_id?id=" + EntityIds + "&max=" + maxRecord,
     };
     await axios(config)
       .then(function (response) {
@@ -75,7 +59,8 @@ export default function PeopleDepartment() {
         setComponentLoader(false);
         if (response.data.length) {
           if (!isScroll) {
-            setEntityInfo(response.data[0]);
+            getEntityInfo(response.data[0].ENTITY_ID);
+            setInfoCardHeaderText(response.data[0].Title);
             setInfoDataLoaded(true);
           }
 
@@ -88,24 +73,21 @@ export default function PeopleDepartment() {
         console.log(error);
       });
   };
+
   const getEntityInfo = async (entity_id) => {
     setInfoDataLoaded(false);
     setNoDataFound(false);
-    var jsonData = {
-      EMPLOYEE_ID: entity_id,
-    };
 
     var config = {
-      method: "post",
-      url: API.API_GET_PEOPLE_DEPARTMENT_INFO,
-      data: jsonData,
+      method: "get",
+      url: API.API_GET_ENTITY_INFO_BY_ENTITY_ID + "?id=" + entity_id,
     };
 
     await axios(config)
       .then(function (response) {
         if (response.data.length) {
-          let entityInfoData = response.data[0];
-          setEntityInfo(entityInfoData);
+          setEntityInfo(response.data);
+
           setInfoDataLoaded(true);
           setNoDataFound(true);
         }
@@ -115,16 +97,10 @@ export default function PeopleDepartment() {
       });
   };
 
-  const handlePageChange = (params) => {
-    let isPrev = params.page === page - 1;
-    setPage(params.page);
-  };
-
   // end    all API call
 
-  const handleEntityInfo = (entity_id) => {
-    setNavTab(0)
-    setValue(0);
+  const handleEntityInfo = (entity_id, entity_name) => {
+    setInfoCardHeaderText(entity_name);
     setInfoDataLoaded(false);
     if (!dataInfoLoaded) {
       notification.toast.warning("Please wait. Your Data is loading...!!");
@@ -137,14 +113,23 @@ export default function PeopleDepartment() {
       event.target.scrollHeight - event.target.scrollTop ===
       event.target.clientHeight;
     if (bottom && dataLoaded) {
-      //   alert('bottom');
-      getEntityList("", entityList?.length, true);
+      getEntityList(entityList?.length, entitiesValue, true);
     }
   };
 
   useEffect(() => {
-    getEntityList();
-  }, []);
+    if (entityData) {
+      let entityIds = entityData
+        .map(function (x) {
+          return x.EXID;
+        })
+        .join(",");
+      if (entityIds) {
+        setEntityIds(entityIds);
+        getEntityList(0, entityIds, false);
+      }
+    }
+  }, [reducerState.applicationData.applicationElements]);
 
   return (
     <div className="page" id="page-department">
@@ -152,45 +137,55 @@ export default function PeopleDepartment() {
         <Grid item lg={3} md={4} xs={12} sm={12}>
           <ApplicationItemFilter
             getEntityList={getEntityList}
+            setEntityIds={setEntityIds}
+            entityData={entityData}
           ></ApplicationItemFilter>
           <div
             className={fixedHeightPaper}
             onScroll={(event) => handleOnScroll(entityList, event)}
           >
             <Box boxShadow={0} className="card bg-secondary" borderRadius={5}>
-              <EntityCard 
-              entityList={entityList}
-              entityInfo={entityInfo}
-              noDataFound={noDataFound}
-              componentLoader={componentLoader}
-              handleEntityInfo={handleEntityInfo}
-              >
-              </EntityCard>
+              <EntityCard
+                entityList={entityList}
+                entityInfo={entityInfo}
+                noDataFound={noDataFound}
+                componentLoader={componentLoader}
+                handleEntityInfo={handleEntityInfo}
+              ></EntityCard>
             </Box>
           </div>
         </Grid>
         <Grid item lg={9} md={8} xs={12} sm={12}>
-          <BasicEntityInfo
-            entityInfo={entityInfo}
-            noDataFound={noDataFound}
-            dataInfoLoaded={dataInfoLoaded}
-          ></BasicEntityInfo>
-
-          <div className={classes.mt_one}>
-            <EntityMainTab
-                  entityInfo={entityInfo}
-                  noDataFound={noDataFound}
-                  dataInfoLoaded={dataInfoLoaded}
-                  loading={loading}
-                  maxCount={maxCount}
-                  activityLogMaxCount={activityLogMaxCount}
-                  handlePageChange={handlePageChange}
-                  navTab={navTab}
-                  setNavTab={setNavTab}
-              ></EntityMainTab>
-       
-            
-          </div>
+          <Grid container>
+            <Grid item xs={12}>
+              {!dataInfoLoaded || entityInfo.length <= 0 ? (
+                <Box
+                  boxShadow={0}
+                  className="card bg-secondary"
+                  borderRadius={5}
+                >
+                  <ComponentLoader type="rect" />
+                  <ComponentLoader type="rect" />
+                </Box>
+              ) : (
+                <Card className={classes.root} className=" bg-secondary">
+                  <CardHeader
+                    style={{
+                      backgroundColor: theme.palette.primary.main,
+                      fontSize: ".9rem",
+                    }}
+                    title={InfoCardHeaderText ? InfoCardHeaderText : ""}
+                  />
+                  <CardContent>
+                    <EntityInfoSection
+                      entityInfoData={entityInfo}
+                      noDataFound={noDataFound}
+                    ></EntityInfoSection>
+                  </CardContent>
+                </Card>
+              )}
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
