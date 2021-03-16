69a73f53090a1722e1b13daea6da44d418e8d10c
Michael Fore <MichaelAF@BoxerProperty.com>
2021-03-16 15:38:44 -0500

    get scheme by typ eon

--- flask/blueprints/utils.py
+++ flask/blueprints/utils.py
@@ -5,3 +5,27 @@ mobile = Mobile('http://home.boxerproperty.com/MobileAPI','michaelaf','Boxer@@20
 
 cases = Cases('https://casesapi.boxerproperty.com')
 
+def xml_to_json(root):
+    fields = []
+    def parser(elem, d={}):
+
+
+        if len(elem)==0:
+            d.update({elem.tag:elem.text})
+            return d
+        else:
+            values = {}
+
+            for i in elem:
+                _  = {}
+                values.update(parser(i,_))
+
+            if elem.tag == 'ASSOC_TYPE':
+                #fields = _.get('ASSOC_TYPE',[])
+                fields.append(values)
+
+
+            return {elem.tag:values}
+    d = parser(root)
+    d.update({'fields':fields})
+    return d
